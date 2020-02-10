57accee54cd4ec98bb7db3085edc33969be9dd38
backlo <hjun4512@gmail.com>
2020-02-10 17:31:38 +0900

    Updates

---
+++ precache-manifest.6c0b2365cba7fe515604083b625e6290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece5c884bbd0b66b76b6252f7f3bac58",
+    "url": "/react_movie/index.html"
+  },
+  {
+    "revision": "0bc13cfba3cea929d938",
+    "url": "/react_movie/static/css/main.fce72787.chunk.css"
+  },
+  {
+    "revision": "78cb638c41b4b0ae9eee",
+    "url": "/react_movie/static/js/2.bb5fdd94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_movie/static/js/2.bb5fdd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc13cfba3cea929d938",
+    "url": "/react_movie/static/js/main.59e91900.chunk.js"
+  },
+  {
+    "revision": "4da468f1767fe98bc0be",
+    "url": "/react_movie/static/js/runtime-main.2741163b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.479c145f265df055800b2a73faa58533.js"
+  "/react_movie/precache-manifest.6c0b2365cba7fe515604083b625e6290.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_movie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
